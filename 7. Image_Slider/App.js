987d55815f35eb515589036b17ad51c2b6e0fc99
ArunaKale2019 <aruna.kale93@gmail.com>
2024-08-04 18:36:20 +0530

    gave all styling to the Image Slide

---
+++ 7. Image_Slider/App.js
@@ -0,0 +1,7 @@
+const left = document.querySelector('.left-arrow-img');
+const right = document.querySelector('.right-arrow-img');
+const slider = document.querySelector('.slider');
+
+right.addEventListener('click',() =>{
+    console.log("hello");
+});
