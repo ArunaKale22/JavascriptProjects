94c6406629fb13bf3c3916338bd1997f5b7c4407
ArunaKale2019 <aruna.kale93@gmail.com>
2024-08-09 18:09:07 +0530

    Worked on right arrow of the image slider- Given its functionality

--- 7. Image_Slider/App.js
+++ 7. Image_Slider/App.js
@@ -1,7 +1,35 @@
-const left = document.querySelector('.left-arrow-img');
-const right = document.querySelector('.right-arrow-img');
-const slider = document.querySelector('.slider');
+const left = document.querySelector(".left-arrow-img");
+const right = document.querySelector(".right-arrow-img");
+const slider = document.querySelector(".img-slider");
+const images = document.querySelectorAll(".Image");
 
-right.addEventListener('click',() =>{
-    console.log("hello");
+//One Method of writing code
+//let slideNumber = 1;
+// right.addEventListener("click", () => {
+//   if (slideNumber < images.length) {
+//     slider.style.transform = `translateX(-${slideNumber * 520}px)`;
+//     slideNumber++;
+//   } else {
+//     slider.style.transform = `translateX(0px)`;
+//     slideNumber = 1;
+//   }
+// });
+
+//Another Way
+
+let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 520}px)`;
+  slideNumber++;
+};
+
+const getFirstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+
+right.addEventListener("click", () => {
+  slideNumber < length ? nextSlide() : getFirstSlide();
 });
