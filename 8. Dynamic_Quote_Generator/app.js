26fb2169686151ac9fa377cc662b8d9b4e56be4a
ArunaKale2019 <aruna.kale93@gmail.com>
2024-09-09 17:19:07 +0530

    added the functionalities in the backend i.e. given the functionalities to New Quote button and the tweet button

--- 8. Dynamic_Quote_Generator/app.js
+++ 8. Dynamic_Quote_Generator/app.js
@@ -1,1 +1,32 @@
-const url = 'https://api.quotable.io/random';
+const apiUrl = "https://api.quotable.io/random";
+const newQuote = document.getElementById("quote");
+const authorName = document.getElementById("author");
+
+async function getQuote(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  // console.log(data);
+  newQuote.innerHTML = data.content;
+  authorName.innerHTML = data.author;
+}
+
+getQuote(apiUrl);
+
+function tweet() {
+  window.open(
+    "https://twitter.com/intent/tweet?text=" + newQuote.innerHTML + 
+    "-----by" + " " + authorName.innerHTML,
+    "Tweet Window",
+    "width =600",
+    "height=300"
+  );
+}
+
+// function whatsapp(){
+//     window.open("https://wa.me/whatsappphonenumber?text=newQuote.innerHTM" +
+//     "-----by" + authorName.innerHTML,
+//     "Tweet Window",
+//     "width =600",
+//     "height=300" 
+// );
+// }
